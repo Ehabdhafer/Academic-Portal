c52611dcc420fae533011d647897a34c5a0fe7ae
unknown <ehabzaferabed@gmail.com>
2024-06-07 10:33:44 +0300

    add user_model

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -1,2 +1,24 @@
-const db = require("../config/db");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 const bcrypt = require("bcrypt");
+
+const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sequelize = require("./config/db");
 const app = express();
 const session = require("express-session");
 const passport = require("passport");
@@ -10,14 +11,20 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// const user_router = require("")
+// const user_router = require("./routes/user_route");
 
 // ---------------------------------------------------------
 
 // app.use(user_router);
 
 // ----------------------------------------------------------
-
-app.listen(5000, () => {
-  console.log("server running at http://localhost:5000");
-});
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("server running at http://localhost:5000");
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
