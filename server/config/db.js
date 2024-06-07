cf67b6056f3c058330ae0fa063f843d3660a72c6
unknown <ehabzaferabed@gmail.com>
2024-06-07 15:13:04 +0300

    add DB config

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require("sequelize");
 
+function customLogger(msg) {
+  if (!msg.startsWith("Executing (default)")) {
+    console.error(msg); // Log only errors or important messages
+  }
+}
+
 const sequelize = new Sequelize("Academy", "Ehab", "1234", {
   host: "localhost",
   dialect: "postgres",
+  logging: customLogger,
 });
 
 module.exports = sequelize;
