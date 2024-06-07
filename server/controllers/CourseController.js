5618db3e24915f4c56c6344d91739852356e5d57
unknown <ehabzaferabed@gmail.com>
2024-06-07 18:50:09 +0300

    edit get courses

--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -1,4 +1,6 @@
 const Courses = require("../models/course_model");
+const { Op } = require("sequelize");
+const User = require("../models/user_model");
 
 exports.postCourse = async (req, res) => {
   const { course_name, description, start_date, end_date } = req.body;
@@ -12,6 +14,26 @@ exports.postCourse = async (req, res) => {
       teacher: user_id,
     });
     return res.status(201).json({ message: "New Course added successfully" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: e.message });
+  }
+};
+
+exports.getCourses = async (req, res) => {
+  try {
+    const result = await Courses.findAll({
+      where: {
+        end_date: {
+          [Op.gte]: new Date(),
+        },
+      },
+      include: {
+        model: User,
+        attributes: ["name"],
+      },
+    });
+    res.json(result);
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: e.message });
--- server/routes/course_route.js
+++ server/routes/course_route.js
@@ -4,5 +4,6 @@ const CourseController = require("../controllers/CourseController");
 const verify = require("../middlewares/authorizationJWT");
 
 router.post("/postcourse", verify.authorize([1]), CourseController.postCourse);
+router.get("/getcourse", CourseController.getCourses);
 
 module.exports = router;
