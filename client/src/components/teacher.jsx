d2013fbb833924f13b991d670e2bd8f0d0b5d515
unknown <ehabzaferabed@gmail.com>
2024-06-08 18:24:14 +0300

    edit teacher page

--- client/src/components/teacher.jsx
+++ client/src/components/teacher.jsx
@@ -2,6 +2,7 @@ import Hero from "./hero";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
 
 const Teacher = () => {
   const token = Cookies.get("token");
@@ -34,6 +35,23 @@ const Teacher = () => {
   return (
     <div>
       <Hero />
+      <Link to={"/addcourse"} className="px-16 ">
+        <button className="inline-flex mb-8 items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          Add Course
+          <svg
+            className="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </Link>
       <div className="lg:px-16">
         {courses ? (
           <div className="flex flex-wrap gap-28">
