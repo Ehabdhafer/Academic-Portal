4a570f784a27503b2fefc93c196e7d19a2dd3624
unknown <ehabzaferabed@gmail.com>
2024-06-08 11:15:16 +0300

    edit header ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/Authcontext";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Header from "./components/header";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Header />
         <div className="App">
-          <Routes>{/* <Route exact path="/" element={<Home />} /> */}</Routes>
+          <Routes>
+            {/* <Route exact path="/" element={<Home />} /> */}
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </AuthProvider>
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -2,98 +2,97 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [menuopen,SetisMenuOpen] = useState(false);
+  const [menuopen, SetisMenuOpen] = useState(false);
 
-    const handleMenu = () => {
-        SetisMenuOpen(!menuopen);
-    };
+  const handleMenu = () => {
+    SetisMenuOpen(!menuopen);
+  };
 
-    return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-xl">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <Link to={'/'}>
-            <button
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-            >
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-xl">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to={"/"}>
+          <button className="flex items-center space-x-3 rtl:space-x-reverse">
             <img
-                src="https://www.clipartmax.com/png/middle/159-1592697_staging-of-signature-business-tourism-events-and-exhibitions-icon-tourism-logo-png.png"
-                className="h-12"
-                alt="Flowbite Logo"
+              src="
+                https://w1.pngwing.com/pngs/800/473/png-transparent-circle-design-adversity-quotient-logo-text-learning-academy-result-purple.png
+              "
+              className="h-12"
+              alt="Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Tourism
+              Academic-Portal
             </span>
-            </button>
-            </Link>
-            <button
-            onClick={handleMenu}
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded={menuopen ? "true" : "false"}
-            >
-            <span className="sr-only">Open main menu</span>
-            <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-            >
-                <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 1h15M1 7h15M1 13h15"
-                />
-            </svg>
-            </button>
-            <div className= {`w-full md:block md:w-auto ${menuopen ? 'block' : 'hidden'}`} id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                <Link to={'/'}>
+          </button>
+        </Link>
+        <button
+          onClick={handleMenu}
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={menuopen ? "true" : "false"}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={`w-full md:block md:w-auto ${
+            menuopen ? "block" : "hidden"
+          }`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link to={"/"}>
                 <button
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    aria-current="page"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
                 >
-                    Home
+                  Home
                 </button>
-                </Link>
-                </li>
-                <li>
-                <Link to={'/about'}>
-                <button
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                    About
+              </Link>
+            </li>
+            <li>
+              <Link to={"#"}>
+                <button className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  About
                 </button>
-                </Link>
-                </li>
-                <li>
-                <Link to={'/login'}>
-                <button
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                    Login
+              </Link>
+            </li>
+            <li>
+              <Link to={"/login"}>
+                <button className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Login
                 </button>
-                </Link>
-                </li>
-                <li>
-                <Link to={'/signup'}>
-                <button
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                    Signup
+              </Link>
+            </li>
+            <li>
+              <Link to={"/signup"}>
+                <button className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Signup
                 </button>
-                </Link>
-                </li>
-            </ul>
-            </div>
+              </Link>
+            </li>
+          </ul>
         </div>
-</nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
