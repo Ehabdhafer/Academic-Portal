8a8901a38e98747579fa46dfbf26c3d19a8cea7e
unknown <ehabzaferabed@gmail.com>
2024-06-08 19:28:40 +0300

    edit header ui

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -7,8 +7,9 @@ const Header = () => {
   const [menuopen, SetisMenuOpen] = useState(false);
   const [user, setUser] = useState(false);
   const navigate = useNavigate();
-  const [cookie] = useCookies(["token"], {
+  const [cookie] = useCookies(["token", "role_id"], {
     token: null,
+    role_id: null,
   });
 
   const handleMenu = () => {
@@ -22,6 +23,15 @@ const Header = () => {
     window.location.reload();
   };
 
+  const handleHomeClick = () => {
+    const roleId = cookie.role_id;
+    if (roleId === 1) {
+      navigate("/teacher");
+    } else {
+      navigate("/");
+    }
+  };
+
   useEffect(() => {
     if (cookie.token !== undefined) {
       setUser(true);
@@ -33,7 +43,7 @@ const Header = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-xl">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to={"/"}>
+        <Link to={"#"}>
           <button className="flex items-center space-x-3 rtl:space-x-reverse">
             <img
               src="
@@ -80,14 +90,13 @@ const Header = () => {
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to={"/"}>
-                <button
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  aria-current="page"
-                >
-                  Home
-                </button>
-              </Link>
+              <button
+                onClick={handleHomeClick}
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                aria-current="page"
+              >
+                Home
+              </button>
             </li>
             <li>
               <Link to={"#"}>
